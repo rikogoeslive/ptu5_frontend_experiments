ce607b8bfe4b4be0b5fd0c69d77ed8277a242c74
Riko <mntsklp@gmail.com>
2022-10-20 15:13:09 +0300

    flask intro

---
+++ flask_intro.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Veikia!"
+
+@app.route("/sveikas<name>")
+def user(name):
+    return f"Sveikas, {name}"
+
+@app.route("/grazi_diena")
+def grazi_diena():
+    return render_template("grazi_diena.html")
+
+@app.route("/zmones")
+def zmones():
+    zmones = ['Justina', 'Darius', 'Ingrida', 'Simas', 'Sergejus', 'Arnoldas']
+    return render_template("zmones.html", zmones=zmones)
+
+if __name__ == "__main__":
+    app.run(debug=True)
